11bfcb81f950c1592868f8a032ff9afe385d5da9
Fabio Percivaldi <fabio.percivaldi@mia-platform.eu>
2022-08-03 00:22:21 +0200

    implemented static response

--- go/api_project.go
+++ go/api_project.go
@@ -11,10 +11,39 @@
 package swagger
 
 import (
+	"encoding/json"
+	"errors"
+	"log"
 	"net/http"
 )
+
+var (
+	errGeneric = errors.New("internal Server Error")
+	// errBadRequest = errors.New("bad Request")
+)
+
+func createProject() ([]byte, error) {
+	response := CreateProjectResponse{
+		Id: "123",
+	}
+	responseBody, err := json.Marshal(response)
+	if err != nil {
+		return nil, err
+	}
+
+	return responseBody, nil
+}
 
 func AddProject(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
+	projectId, err := createProject()
+
+	if err != nil {
+		log.Fatalln("failed response unmarshalling")
+		http.Error(w, errGeneric.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(projectId)
 }
--- go/model_project.go
+++ go/model_project.go
@@ -11,10 +11,13 @@
 package swagger
 
 type Project struct {
-
 	Id string `json:"id,omitempty"`
 
 	Client string `json:"client,omitempty"`
 
 	Type_ string `json:"type,omitempty"`
+}
+
+type CreateProjectResponse struct {
+	Id string `json:"id,omitempty"`
 }
